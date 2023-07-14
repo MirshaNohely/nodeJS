3e7ee05070c464ddfabb7cd75a9b63448acf0a18
Mirsha Cortés <72098013+MirshaNohely@users.noreply.github.com>
2023-07-14 01:06:51 -0600

    Update usuarioRautes.js

--- routes/usuarioRautes.js
+++ routes/usuarioRautes.js
@@ -2,12 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/', function(req, res)  {
+router.get('/', function(req, res){
     res.json({msg: 'Hola Mundo en express'})
 });
 
-router.get('/nosotros', function(req, res)  {
-    res.json({msg: 'Respuesta de tipo post'})
+router.get('/karma', function(req, res){
+    res.json({msg: 'And I Keep My Side On The Street Clean, You Wouldnt Know What I Mean'})
 });
 
 export default router
